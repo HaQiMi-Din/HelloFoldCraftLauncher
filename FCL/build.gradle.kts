72e88cb77d4f132bfb2896ab65718cb03be3fc79
HaQiMi-Din <3914317526@qq.com>
2026-08-24 15:52:53 +0800

    Update build.gradle.kts

--- FCL/build.gradle.kts
+++ FCL/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 android {
-    namespace = "com.tungsten.hfcl"
+    namespace = "com.tungsten.fcl"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     var localProperty: Properties? = null
